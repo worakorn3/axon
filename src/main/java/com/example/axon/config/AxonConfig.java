45453b516dfb0bd286eb8f690211cb101ce99048
BuildTools <unconfigured@null.spigotmc.org>
2023-02-18 21:31:54 +0700

    config more detail

--- src/main/java/com/example/axon/config/AxonConfig.java
+++ src/main/java/com/example/axon/config/AxonConfig.java
@@ -1,6 +1,10 @@
 package com.example.axon.config;
 
 import com.example.axon.aggregate.OrderAggregate;
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.DuplicateCommandHandlerResolver;
+import org.axonframework.commandhandling.SimpleCommandBus;
+import org.axonframework.common.AxonThreadFactory;
 import org.axonframework.common.caching.Cache;
 import org.axonframework.common.caching.WeakReferenceCache;
 import org.axonframework.config.ConfigurerModule;
@@ -8,8 +12,12 @@ import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventsourcing.EventSourcingRepository;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.messaging.annotation.ParameterResolverFactory;
+import org.axonframework.tracing.SpanFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 public class AxonConfig {
@@ -32,10 +40,29 @@ public class AxonConfig {
 
     @Bean
     public ConfigurerModule processorDefaultConfigurerModule() {
-        EventProcessingConfigurer.PooledStreamingProcessorConfiguration psepConfig =
-                (config, builder) -> builder.initialSegmentCount(32);
+        EventProcessingConfigurer.PooledStreamingProcessorConfiguration psepConfig = (config, builder) -> builder.initialSegmentCount(32)
+                .batchSize(100)
+                .coordinatorExecutor(Executors.newScheduledThreadPool(1, new AxonThreadFactory("Axon-Coordinator[" + builder.name() + "]")))
+                .workerExecutor(Executors.newScheduledThreadPool(100, new AxonThreadFactory("Axon-Worker[" + builder.name() + "]")))
+                .tokenClaimInterval(5000)
+                .claimExtensionThreshold(5000);
 
-        return configurer -> configurer.eventProcessing(conf -> conf.registerPooledStreamingEventProcessorConfiguration(psepConfig));
+        return configurer -> configurer.eventProcessing(conf -> conf.usingPooledStreamingEventProcessors().registerPooledStreamingEventProcessorConfiguration(psepConfig));
     }
 
+    @Qualifier("localSegment")
+    @Bean
+    public SimpleCommandBus commandBus(
+            org.axonframework.config.Configuration configuration,
+            DuplicateCommandHandlerResolver resolver,
+            SpanFactory factory
+    ) {
+        SimpleCommandBus bus = SimpleCommandBus.builder()
+                .duplicateCommandHandlerResolver(resolver)
+                .messageMonitor(configuration.messageMonitor(CommandBus.class, "commandBus"))
+                .spanFactory(factory)
+                .build();
+
+        return bus;
+    }
 }
